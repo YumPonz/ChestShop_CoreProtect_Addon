582338d4ad60c1400c04da0208b5d1062e7e87a1
shin19 <github@shin19.net>
2021-07-08 00:24:54 +0900

    Release v1.0.0

---
+++ src/main/java/com/github/shin19/ChestShop_CoreProtect_Addon.java
@@ -0,0 +1,52 @@
+package com.github.shin19;
+
+import com.Acrobot.ChestShop.Events.TransactionEvent;
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChestShop_CoreProtect_Addon extends JavaPlugin implements Listener {
+    private CoreProtectAPI coreProtect = null;
+
+    @Override
+    public void onEnable() {
+        coreProtect = getCoreProtect();
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    @EventHandler
+    public void onChestTrade(TransactionEvent event) {
+        if (coreProtect != null) {
+            coreProtect.logContainerTransaction(event.getClient().getDisplayName(), event.getOwnerInventory().getLocation());
+        }
+    }
+
+    private CoreProtectAPI getCoreProtect() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("CoreProtect");
+
+        // Check that CoreProtect is loaded
+        if (!(plugin instanceof CoreProtect)) {
+            return null;
+        }
+
+        // Check that the API is enabled
+        CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
+        if (!CoreProtect.isEnabled()) {
+            return null;
+        }
+
+        // Check that a compatible version of the API is loaded
+        if (CoreProtect.APIVersion() < 6) {
+            return null;
+        }
+
+        return CoreProtect;
+    }
+}
